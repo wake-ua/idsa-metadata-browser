4b53374e630b89691f004e6a36ae43fde50355dd
espona <lucia.espona@gmail.com>
2024-07-10 12:21:39 +0200

    First version of the browser

---
+++ main.py
@@ -0,0 +1,209 @@
+import os
+import json
+import requests
+from dotenv import load_dotenv
+from persistence import PersistenceConfiguration, Persistence
+
+load_dotenv('.env')
+
+METADATA_BROKER_URLS = os.getenv("METADATA_BROKER_URLS").split(',')
+METADATA_BROKER_DOCKER_URL = os.getenv("METADATA_BROKER_DOCKER_URL")
+CONNECTOR_URL = os.getenv("CONNECTOR_URL")
+CONNECTOR_USER = os.getenv('CONNECTOR_USER')
+CONNECTOR_PW = os.getenv('CONNECTOR_PW')
+
+DB_URI = os.getenv('DB_URI')
+DB_NAME = os.getenv('DB_NAME')
+BROKER_COLLECTION = os.getenv('BROKER_COLLECTION')
+PROVIDER_COLLECTION = os.getenv('PROVIDER_COLLECTION')
+RESOURCE_COLLECTION = os.getenv('RESOURCE_COLLECTION')
+CATALOG_COLLECTION = os.getenv('CATALOG_COLLECTION')
+
+
+# Get broker description
+def get_broker_description(metadata_broker_url: str) -> dict:
+    response = requests.get(metadata_broker_url, verify=False)
+    print(" \t * Request GET {0} \t => {1}".format(metadata_broker_url, response.status_code))
+    content = response.content
+    description = json.loads(content)
+
+    catalog_lists = []
+    for resourceCatalog in description.get("ids:resourceCatalog", []):
+        for offeredResource in resourceCatalog.get("ids:offeredResource", []):
+            for representation in offeredResource.get("ids:representation", []):
+                for instance in representation.get("ids:instance", []):
+                    catalog_list = instance.get("@id")
+                    if catalog_list:
+                        catalog_lists += [catalog_list]
+    description["_catalog_lists"] = list(set(catalog_lists))
+    return description
+
+
+def get_broker_catalogs(metadata_broker_url: str,broker_catalog_lists: list,  connector_url: str, auth: tuple) -> list:
+    catalogs = {}
+
+    for catalog in broker_catalog_lists:
+        request_url = "{0}/api/ids/description?recipient={1}&elementId={2}"\
+                      .format(connector_url, metadata_broker_url, catalog)
+        response = requests.post(request_url, data={}, auth=auth, verify=False)
+        print(" \t * Request POST {0} \t => {1}".format(request_url, response.status_code))
+        content = response.content
+        broker_graph = json.loads(content).get('@graph', [])
+        for element in broker_graph:
+            if element.get("@type") == "ids:BaseConnector" and element.get("@id"):
+                if catalog not in catalogs.keys():
+                    catalogs[catalog] = []
+                catalogs[catalog] += [{"@id": element.get("@id")}]
+    broker_catalogs = [{'@id': c, '@type': "ids:BaseConnector", 'connectors': l} for c, l in catalogs.items()]
+    return broker_catalogs
+
+
+def get_broker_connectors(metadata_broker_url: str, broker_catalogs: list, connector_url: str, auth: tuple) -> list:
+    catalogs_wit_urls = []
+
+    for catalog in broker_catalogs:
+        connectors_with_urls = []
+        for connector in catalog['connectors']:
+            connector_id = connector['@id']
+            request_url = "{0}/api/ids/description?recipient={1}&elementId={2}"\
+                          .format(connector_url, metadata_broker_url, connector_id)
+            response = requests.post(request_url, data={}, auth=auth, verify=False)
+            print(" \t * Request POST {0} \t => {1}".format(request_url, response.status_code))
+            content = response.content
+            broker_graph = json.loads(content).get('@graph', [])
+            for element in broker_graph:
+                if element.get("@type") and element.get("@type") == "ids:ConnectorEndpoint" \
+                        and element.get("accessURL"):
+                    connector["accessURL"] = str(element.get("accessURL"))
+                    connectors_with_urls += [connector]
+        catalog['connectors'] = connectors_with_urls
+        catalogs_wit_urls += [catalog]
+
+    return catalogs_wit_urls
+
+
+def get_provider_description(broker_doc: dict, connector_url: str, auth: tuple) -> list:
+    providers = []
+
+    for broker_catalog in broker_doc['_broker_catalogs']:
+        for broker_connector in broker_catalog['connectors']:
+            provider_url = broker_connector['accessURL']
+            request_url = "{0}/api/ids/description?recipient={1}".format(connector_url, provider_url)
+            response = requests.post(request_url, data={}, auth=auth, verify=False)
+            print(" \t * Request POST {0} \t => {1}".format(request_url, response.status_code))
+            provider = json.loads(response.content)
+            provider["_broker_id"] = broker_doc['@id']
+            provider["_broker_catalog_id"] = broker_catalog['@id']
+            provider["_broker_connector_id"] = broker_connector['@id']
+            provider["_provider_url"] = provider_url
+            resource_catalogs = provider.get('ids:resourceCatalog', [])
+            provider["_catalogs"] = []
+            for element in resource_catalogs:
+                if element.get("@type") and element.get("@type") == "ids:ResourceCatalog" and element.get("@id"):
+                    provider["_catalogs"] = [{"@id": str(element.get("@id")), "@type": "ids:ResourceCatalog"}]
+            providers += [provider]
+
+    return providers
+
+
+def get_provider_catalog_description(provider_docs: list, connector_url: str, auth: tuple) -> (list, list):
+    catalogs = []
+    resources = []
+
+    for provider in provider_docs:
+        provider_url = provider["_provider_url"]
+        for provider_catalog in provider["_catalogs"]:
+            provider_catalog_id = provider_catalog['@id']
+            request_url = "{0}/api/ids/description?recipient={1}&elementId={2}".format(connector_url, provider_url,
+                                                                                       provider_catalog_id)
+            response = requests.post(request_url, data={}, auth=auth, verify=False)
+            print(" \t * Request POST {0} \t => {1}".format(request_url, response.status_code))
+            content = response.content
+            catalog = json.loads(content)
+
+            catalog["_provider_id"] = provider['@id']
+            for k in ["_broker_id", "_broker_catalog_id", "_broker_connector_id", "_provider_url"]:
+                catalog[k] = provider[k]
+
+            catalog_resources = catalog["ids:offeredResource"]
+            catalog["ids:offeredResource"] = [str(r['@id']) for r in catalog_resources]
+            catalogs += [catalog]
+
+            for resource in catalog_resources:
+                for k in ["_broker_id", "_broker_catalog_id", "_broker_connector_id", "_provider_url", "_provider_id"]:
+                    resource[k] = catalog[k]
+                resource["_catalog_id"] = str(catalog['@id'])
+                resources += [resource]
+    return catalogs, resources
+
+
+def init_persistence(db_uri: str = DB_URI, db_name: str = DB_NAME,
+                     broker_collection_name: str = BROKER_COLLECTION,
+                     provider_collection_name: str = PROVIDER_COLLECTION,
+                     catalog_collection_name: str = CATALOG_COLLECTION,
+                     resource_collection_name: str = RESOURCE_COLLECTION) -> Persistence:
+    config = PersistenceConfiguration(db_uri, db_name, broker_collection_name, provider_collection_name,
+                                      catalog_collection_name, resource_collection_name)
+
+    persistence = Persistence(config)
+    return persistence
+
+
+def main(metadata_broker_urls: str = METADATA_BROKER_URLS, metadata_broker_docker_url: str = METADATA_BROKER_DOCKER_URL,
+         connector_url: str = CONNECTOR_URL, connector_user: str = CONNECTOR_USER, connector_pw: str = CONNECTOR_PW,
+         db_uri: str = DB_URI, db_name: str = DB_NAME):
+
+    persistence = init_persistence()
+
+    requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
+
+    for metadata_broker_url in metadata_broker_urls:
+        # Use a breakpoint in the code line below to debug your script.
+        print('Metadata Browser started... \n Setup:')  # Press ⌘F8 to toggle the breakpoint.
+        print('\t - METADATA_BROKER_URL: {0}'.format(metadata_broker_url))  # Press ⌘F8 to toggle the breakpoint.
+        print('\t - DB_URI: {0}, DB_NAME: {1}'.format(db_uri, db_name))  # Press ⌘F8 to toggle the breakpoint.
+
+        print("\n 0. Requesting broker self-description...")
+        broker_description = get_broker_description(metadata_broker_url)
+        broker_doc = persistence.save_broker(broker_description)
+        print("\t - Saved broker '{}' ({}, {})".format(broker_doc["_name"], broker_doc["@id"], broker_doc["_id"]))
+        print("\t - Got {} catalog list(s) ({}...)".format(len(broker_doc["_catalog_lists"]),
+                                                           ', '.join(broker_doc["_catalog_lists"])[:50]))
+        print("\n 1. Query broker list of catalog(s)...")
+        broker_catalog_lists = broker_doc["_catalog_lists"]
+        connector_auth = (connector_user, connector_pw)
+        broker_doc["_broker_catalogs"] = get_broker_catalogs(metadata_broker_docker_url, broker_catalog_lists,
+                                                             connector_url, connector_auth)
+        print("\t - Got {} catalog(s) ({}...)".format(len(broker_doc["_broker_catalogs"]),
+                                                      str(broker_doc["_broker_catalogs"])[:200]))
+        broker_doc = persistence.save_broker(broker_doc)
+
+        print("\n 2. Request broker connectors info...")
+        broker_catalogs = broker_doc["_broker_catalogs"]
+        broker_doc["_broker_catalogs"] = get_broker_connectors(metadata_broker_docker_url, broker_catalogs,
+                                                connector_url, connector_auth)
+
+        print("\t - Got {} catalog(s) with connector(s) ({}...)".format(len(broker_doc["_broker_catalogs"]),
+                                                                        str(broker_doc["_broker_catalogs"])[:200]))
+        broker_doc = persistence.save_broker(broker_doc)
+
+        print("\n 3. Request each provider self-description...")
+        provider_docs = get_provider_description(broker_doc, connector_url, connector_auth)
+        print("\t - Got {} provider(s) description(s) ({}...)".format(len(provider_docs),
+                                                                      str(provider_docs)[:200]))
+        provider_docs = persistence.save_providers(provider_docs)
+
+        print("\n 4. Request each provider catalog description...")
+        catalog_docs, resource_docs = get_provider_catalog_description(provider_docs, connector_url, connector_auth)
+        catalog_docs = persistence.save_catalogs(catalog_docs)
+        resource_docs = persistence.save_resources(resource_docs)
+        print("\t - Got {} provider catalog(s) description(s) ({}...)".format(len(catalog_docs),
+                                                                              str(catalog_docs)[:500]))
+        print("\t - Got {} provider resource(s) ({}...)".format(len(resource_docs), str(resource_docs)[:500]))
+
+        print("\t... DONE.")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
---
+++ persistence.py
@@ -0,0 +1,88 @@
+from datetime import datetime
+from pymongo import MongoClient
+from pymongo.collection import Collection
+from pymongo.server_api import ServerApi
+import certifi
+
+
+class PersistenceConfiguration:
+
+    def __init__(self, db_uri: str, db_name: str,
+                 broker_collection_name: str, provider_collection_name: str, catalog_collection_name: str,
+                 resource_collection_name: str):
+        self.db_uri = db_uri
+        self.db_name = db_name
+        self.broker_collection_name = broker_collection_name
+        self.provider_collection_name = provider_collection_name
+        self.catalog_collection_name = catalog_collection_name
+        self.resource_collection_name = resource_collection_name
+
+
+class Persistence:
+    def __init__(self, config: PersistenceConfiguration):
+        self.config = config
+        self.client = MongoClient(self.config.db_uri, server_api=ServerApi('1'), tlsCAFile=certifi.where())
+        self.db = self.client[self.config.db_name]
+        self.test_connection()
+
+        self.broker_collection = self.db[self.config.broker_collection_name]
+        self.provider_collection = self.db[self.config.provider_collection_name]
+        self.catalog_collection = self.db[self.config.catalog_collection_name]
+        self.resource_collection = self.db[self.config.resource_collection_name]
+
+    def test_connection(self, test_collection: str = 'test') -> bool:
+        try:
+            self.client.admin.command('ping')
+            self.db[test_collection].replace_one({'success': True}, {'success': True}, upsert=True)
+            doc = self.db[test_collection].find_one()
+            print("* Connection to database '{}' => OK {}".format(self.db.name, doc))
+            return True
+        except Exception as e:
+            raise Exception('ERROR: Cannot connect to db {}/{}, {}'.format(self.config.db_uri, self.config.db_name, e))
+
+    @staticmethod
+    def timestamp(doc: dict, collection: Collection, id_query: dict) -> dict:
+        existing_doc = collection.find_one(id_query)
+        if existing_doc:
+            doc['_insert_timestamp'] = existing_doc["_insert_timestamp"]
+        else:
+            doc['_insert_timestamp'] = datetime.now()
+        doc['_update_timestamp'] = datetime.now()
+        return doc
+
+    def save_broker(self, doc: dict) -> dict:
+        id_query = {"@id": doc["@id"]}
+        doc['_name'] = ([d.get('@value') for d in doc.get('ids:title', [])
+                        if d.get('@language') == 'en'] + ['Unnamed Broker'])[0]
+        return self.save_doc(doc, self.broker_collection, id_query)
+
+    def save_providers(self, docs: list) -> list:
+        inserted_docs = [self.save_provider(doc) for doc in docs]
+        return inserted_docs
+
+    def save_provider(self, doc: dict) -> dict:
+        doc['_name'] = ([d.get('@value') for d in doc.get('ids:title', [])] + ['Unnamed Provider'])[0]
+        id_query = {"@id": doc["@id"]}
+        return self.save_doc(doc, self.provider_collection, id_query)
+
+    def save_catalogs(self, docs: list) -> list:
+        inserted_docs = [self.save_catalog(doc) for doc in docs]
+        return inserted_docs
+
+    def save_catalog(self, doc: dict) -> dict:
+        id_query = {"@id": doc["@id"]}
+        return self.save_doc(doc, self.catalog_collection, id_query)
+
+    def save_resources(self, docs: list) -> list:
+        inserted_docs = [self.save_resource(doc) for doc in docs]
+        return inserted_docs
+
+    def save_resource(self, doc: dict) -> dict:
+        id_query = {"@id": doc["@id"]}
+        return self.save_doc(doc, self.resource_collection, id_query)
+
+    def save_doc(self, doc: dict, collection: Collection, id_query: dict):
+        doc = self.timestamp(doc, collection, id_query)
+        collection.replace_one(id_query, doc, upsert=True)
+        inserted_doc = collection.find_one(id_query)
+        return inserted_doc
